c4101d4343ad4383d35ef51bfb84e79c4cae6d71
RadityoXYZ <51162871+RadityoXYZ@users.noreply.github.com>
2019-09-13 18:00:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.TOKEN = 'NTgzMTA1OTc2MDYwNzM5NTg0.XPDXQQ.6C459nG98sPPH7Us_35Zwqwg2t0';
 
-exports.PREFIX = '=';
+exports.PREFIX = 'xyz';
 
 exports.GOOGLE_API_KEY = 'AIzaSyAn8APSUyfMpCNimzuYlaigdY27wquzKjk';
